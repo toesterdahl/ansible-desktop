f1d0f2b3bf75bff5958493fbb929985318741ebf
Torbjörn Österdahl <toesterdahl@ultra-marine.org>
2021-01-10 09:10:41 +0100

    Syncing private variables from bin

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# This will sync some things that does not go into the public repo
+# (excluded in git)
+#  - inventory.yml
+#  - variables
+#
+rsync -av ~/bin/ansible-desktop/ ~/wsp/ansible-desktop
+
 ansible-playbook main.yml --ask-become-pass --limit `hostname` --inventory inventory.yml
 
